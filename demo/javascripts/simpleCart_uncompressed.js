d8d780bd23373354eb825a5f4b2962b679722014
jasonudoo <jasonudoo@gmail.com>
2013-05-15 08:37:34 -0700

    modifiy mycart page

--- demo/javascripts/simpleCart_uncompressed.js
+++ demo/javascripts/simpleCart_uncompressed.js
@@ -373,7 +373,7 @@ function cart( email ) {
 			j = counter + 1; 
 			strn = strn + 	"&item_name_"    + j + "=" + tempItem.getValue('name') +
 		                	"&item_number_"  + j + "=" + j +
-		                	"&quantity_"     + j + "=" + 1 +
+		                	"&quantity_"     + j + "=" + "1" +
 		                	"&amount_"       + j + "=" + this.returnFormattedPrice(tempItem.getValue('price') ) +
 		      				"&no_shipping_"  + j + "=" + "0" +
 							"&no_note_"   	 + j + "=" + "1";
