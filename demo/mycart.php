eea1cfaef61b027bde0679b01602b146fbd2cd99
jasonudoo <jasonudoo@gmail.com>
2013-05-15 07:51:37 -0700

    modifiy mycart page

--- demo/mycart.php
+++ demo/mycart.php
@@ -42,6 +42,8 @@ $mem_id = isset($_REQUEST['uid']) ? intval($_REQUEST['uid']) : 331;
 		.cartHeaders div{
 			color:#333;
 		}
+		
+        .left_title { font-size:20px; color:#DDDDDD; font-weight:bold }
 		.simpleCart_item{
 			float:left;
 			clear:left;
@@ -143,13 +145,13 @@ while($row = mysql_fetch_assoc($result))
 	}
 	if($mem_id = 333 && $row['virtuemart_category_id'] == 11)
 	{
-		echo "<span class='label-txt'><a href='demo.php?uid=" . $mem_id . "&cid=". $row['virtuemart_category_id'] . "'>" . $row['category_name'] ."</a></span>";
+		echo "<span class='left_title'><a href='demo.php?uid=" . $mem_id . "&cid=". $row['virtuemart_category_id'] . "'>" . $row['category_name'] ."</a></span>";
 		break;
 	}		
 }
 echo "<br/>";
 echo "<br/>";
-echo "<h3>Package Price : CHF" . $cartInfo['cart_total'] . "</h3>";
+echo "<h4>Package Price : CHF" . $cartInfo['cart_total'] . "</h4>";
 echo "<br/>";
 echo "<span>Leider ist es nicht möglich, einen Einkauf zu tätigen, welcher unter dem Standardbetrag der Gemüsetasche liegt. Bitte ziehen Sie weitere Produkte in Ihre Tasche. Vielen Dank! </span>"; 
 ?>
